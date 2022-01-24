29c5d40e590e8236d37802bf288202862b0d82cf
Silvano Seva <silseva@fastwebnet.it>
2022-01-24 22:11:12 +0100

    Configured miosix kernel for STM32F411VE discovery

--- miosix/config/miosix_settings.h
+++ miosix/config/miosix_settings.h
@@ -24,7 +24,7 @@
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
- ***************************************************************************/ 
+ ***************************************************************************/
 
 #ifndef MIOSIX_SETTINGS_H
 #define MIOSIX_SETTINGS_H
@@ -34,7 +34,7 @@
 // The PARSING_FROM_IDE is because Netbeans gets confused by this, it is never
 // defined when compiling the code.
 #ifndef PARSING_FROM_IDE
-#error This error is a reminder that you have not edited miosix_settings.h yet.
+// #error This error is a reminder that you have not edited miosix_settings.h yet.
 #endif //PARSING_FROM_IDE
 
 /**
@@ -85,20 +85,20 @@ namespace miosix {
 /// \def WITH_FILESYSTEM
 /// Allows to enable/disable filesystem support to save code size
 /// By default it is defined (filesystem support is enabled)
-#define WITH_FILESYSTEM
+// #define WITH_FILESYSTEM
 
 /// \def WITH_DEVFS
 /// Allows to enable/disable DevFs support to save code size
 /// By default it is defined (DevFs is enabled)
-#define WITH_DEVFS
-    
+// #define WITH_DEVFS
+
 /// \def SYNC_AFTER_WRITE
 /// Increases filesystem write robustness. After each write operation the
 /// filesystem is synced so that a power failure happens data is not lost
 /// (unless power failure happens exactly between the write and the sync)
 /// Unfortunately write latency and throughput becomes twice as worse
 /// By default it is defined (slow but safe)
-#define SYNC_AFTER_WRITE
+// #define SYNC_AFTER_WRITE
 
 /// Maximum number of open files. Trying to open more will fail.
 /// Cannot be lower than 3, as the first three are stdin, stdout, stderr
